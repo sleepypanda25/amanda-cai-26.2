ce4da8f18e4aecd00b60a2958b6f2187ab23b359
Your Name <yourname@example.com>
2026-06-11 15:15:24 -0700

    week 11 changes

--- js/cat.js
+++ js/cat.js
@@ -1,5 +1,7 @@
+// api key
 const api_key = "live_eQz4jg8FjS2mAigaK9iRmwiQRbv16polxYxgpngAx5WJxGHrEVWuFvjcR0IQPcSm";
 
+// fetch 18 random cat images
 fetch(`https://api.thecatapi.com/v1/images/search?limit=18`, {
     headers: {
         'x-api-key': api_key
@@ -9,16 +11,20 @@ fetch(`https://api.thecatapi.com/v1/images/search?limit=18`, {
         return response.json();
     })
     .then((data) => {
+        // retrieve images
         let imagesData = data;
         imagesData.map(function(imageData) {
+            // create image
             let image = document.createElement('img');
             image.src = `${imageData.url}`;
 
+            // create grid cell
             let gridCell = document.createElement('div');
             gridCell.classList.add('col');
             gridCell.classList.add('col-lg');
             gridCell.appendChild(image)
 
+            // add image
             document.getElementById('grid').appendChild(gridCell);
         })
     })
@@ -26,6 +32,7 @@ fetch(`https://api.thecatapi.com/v1/images/search?limit=18`, {
         console.log(error)
     })
 
+// fetch ragdoll cat image
 fetch(`https://api.thecatapi.com/v1/images/oGefY4YoG`, {
     headers: {
         'x-api-key': api_key
@@ -35,9 +42,12 @@ fetch(`https://api.thecatapi.com/v1/images/oGefY4YoG`, {
         return response.json();
     })
     .then((data) => {
+        // retrieve image
         let imageData = data;
         let image = document.createElement('img');
         image.src = `${imageData.url}`;
+
+        // add image
         document.getElementById('sort-breed').appendChild(image);
     })
     .catch(function(error) {
--- js/index.js
+++ js/index.js
@@ -1,31 +1,43 @@
+// --- FOOTER ---
+
+// make footer section
 const footerElement = document.createElement("footer");
 footerElement.className = "footer";
 document.body.appendChild(footerElement);
 
+// footer date
 const today = new Date();
 const thisYear = today.getFullYear();
 
+// footer text
 const footer = document.querySelector(".footer");
-
 const copyright = document.createElement("p");
 copyright.className = "copyright";
 copyright.innerHTML = `&copy; Amanda Cai ${thisYear}`;
 footer.appendChild(copyright);
 
+// --- SKILLS ---
+
+// make skills section
 const skills = ["Java", "Python", "C", "JavaScript", "HTML", "CSS", "GitHub"];
 const skillsSection = document.querySelector(".skills");
 const skillsList = skillsSection.querySelector(".skills-list");
 
+// skills list
 for(let i = 0; i < skills.length; i++) {
     const skill = document.createElement("li");
     skill.innerText = `${skills[i]}`;
     skillsList.appendChild(skill);
 }
 
+// --- MESSAGE FORM & LIST---
+
+// retrieve messageForm
 const messageForm = document.querySelector('[name="leave_message"]');
 messageForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    // read user info
     const usersName = event.target.usersName.value;
     const usersEmail = event.target.usersEmail.value;
     const usersMessage = event.target.usersMessage.value;
@@ -34,10 +46,15 @@ messageForm.addEventListener('submit', function(event) {
     console.log(`User's Email: ${usersEmail}`);
     console.log(`User's Message: ${usersMessage}`);
 
+    // messages list
     const messageSection = document.querySelector("#messages");
     const messageList = messageSection.querySelector("ul");
     const newMessage = document.createElement("li");
+    
+    // clickable email link + name
     newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a><p><span>${usersMessage}</span></p>`;
+    
+    // remove button
     const removeButton = document.createElement("button");
     removeButton.innerText = "remove";
     removeButton.type = "button";
@@ -45,12 +62,18 @@ messageForm.addEventListener('submit', function(event) {
         const entry = removeButton.parentNode;
         entry.remove();
     });
+
+    // append components
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
 
+    // reset form
     messageForm.reset();
 })
 
+// --- PROJECTS ---
+
+// fetch from GitHub repositories
 fetch('https://api.github.com/users/sleepypanda25/repos')
     .then(response => {
         if (!response.ok) {
@@ -59,12 +82,15 @@ fetch('https://api.github.com/users/sleepypanda25/repos')
         return response.json();
     })
     .then(data => {
+        //retrieve repositories
         const repositories = data;
         console.log(repositories);
 
+        // make projects section
         const projectSection = document.querySelector("#Projects");
         const projectList = projectSection.querySelector("ul");
 
+        // add repositories to list
         for (let i = 0; i < repositories.length; i++) {
         const project = document.createElement("li");
         project.innerText = repositories[i].name;
